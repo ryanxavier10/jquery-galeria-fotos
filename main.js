a0c14653f770ac2fc52f9c40f4c0da33531bded4
Ryan Xavier <lugialga@gmail.com>
2024-02-03 12:34:42 -0300

    correção de um erro no código

--- main.js
+++ main.js
@@ -11,11 +11,12 @@ $(document).ready(function(){
         e.preventDefault();
         const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
         const novoItem = $('<li style="display: none"></li>');
-        $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
         <div class="overlay-imagem-link">
-        <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver a imagem em tamanho real"
-        Ver imagem em tamanho real
+            <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver a imagem em tamanho real">
+                Ver imagem em tamanho real
+                </a>
         </div>
         `).appendTo(novoItem);
         $(novoItem).appendTo('ul');
